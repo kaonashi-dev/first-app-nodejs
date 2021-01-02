3c549b397511c78b9ddbbe5152e22f119738747c
samueldlh <samueldlh.dev@gmail.com>
2021-01-01 23:36:33 -0500

    views

--- src/server.js
+++ src/server.js
@@ -6,13 +6,13 @@ const app = express();
 const config = require('./config');
 const router = require('./routes/router');
 
-app.set('view engine', 'pug');
+/// Configuración
 app.set('views', path.join(__dirname, '/views'));
-
+app.set('view engine', 'pug');
+app.engine('js',require('pug').renderFile);
 app.use(express.static(path.join(__dirname, '/public')));
 
+/// Routes
 app.use(router);
 
-app.listen(config.port, (req, res) => {
-    console.log(`Server listening on port ${config.port}`);
-});
+app.listen(config.port, (req, res) => console.log(`Server listening on port ${config.port}`));
